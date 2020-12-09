56b68c86754e52f020037e5d8d74f62216596b1f
JJ Lehmann <jonathan.lehmann@guardicore.com>
2020-12-09 14:12:34 +0200

    added port names

--- internal/collection/utils.go
+++ internal/collection/utils.go
@@ -1,7 +1,9 @@
 package collection
 
 import (
+    "fmt"
     "strings"
+    "honnef.co/go/netdb"
 )
 
 func nullStr(s []byte) string {
@@ -10,4 +12,16 @@ func nullStr(s []byte) string {
 
 func commStr(comm [16]byte) string {
     return nullStr(comm[:])
+}
+
+func servName(proto uint8, port uint16) string {
+    protoEnt := netdb.GetProtoByNumber((int)(proto))
+    if protoEnt == nil {
+        return fmt.Sprintf("%d", port)
+    }
+    servEnt := netdb.GetServByPort((int)(port), protoEnt)
+    if servEnt == nil {
+        return fmt.Sprintf("%d/%s", port, protoEnt.Name)
+    }
+    return servEnt.Name
 }
